e698011f08d1248c09b3329feaf1503d53886265
sonixAnder <148888208+sonixAnder@users.noreply.github.com>
2024-12-04 19:37:37 +0300

    Add files via upload

---
+++ The Merze Alphabet/The Merze Alphabet/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MorseCodeTranslator
+{
+    public class MorseTranslator
+    {
+        private static readonly Dictionary<char, string> TextToMorse = new Dictionary<char, string>
+        {
+            { 'A', ".-" },
+            { 'B', "-..." },
+            { 'C', "-.-." },
+            { 'D', "-.." },
+            { 'E', "." },
+            { 'F', "..-." },
+            { 'G', "--." },
+            { 'H', "...." },
+            { 'I', ".." },
+            { 'J', ".---" },
+            { 'K', "-.-" },
+            { 'L', ".-.." },
+            { 'M', "--" },
+            { 'N', "-." },
+            { 'O', "---" },
+            { 'P', ".--." },
+            { 'Q', "--.-" },
+            { 'R', ".-." },
+            { 'S', "..." },
+            { 'T', "-" },
+            { 'U', "..-" },
+            { 'V', "...-" },
+            { 'W', ".--" },
+            { 'X', "-..-" },
+            { 'Y', "-.--" },
+            { 'Z', "--.." },
+            { '1', ".----" },
+            { '2', "..---" },
+            { '3', "...--" },
+            { '4', "....-" },
+            { '5', "....." },
+            { '6', "-...." },
+            { '7', "--..." },
+            { '8', "---.." },
+            { '9', "----." },
+            { '0', "-----" },
+            { ' ', "/" }
+        };
+        private static readonly Dictionary<string, char> MorseToText = new Dictionary<string, char>();
+        static MorseTranslator()
+        {
+            foreach (var pair in TextToMorse)
+            {
+                MorseToText[pair.Value] = pair.Key;
+            }
+        }
+        public static string ToMorse(string input)
+        {
+            input = input.ToUpper();
+            var morse = new List<string>();
+
+            foreach (char c in input)
+            {
+                if (TextToMorse.ContainsKey(c))
+                {
+                    morse.Add(TextToMorse[c]);
+                }
+                else
+                {
+                    throw new ArgumentException($"Символ {c} не поддерживается.");
+                }
+            }
+            return string.Join(" ", morse);
+        }
+        public static string FromMorse(string input)
+        {
+            var text = new List<char>();
+            var morseSymbols = input.Split(' ');
+
+            foreach (string symbol in morseSymbols)
+            {
+                if (MorseToText.ContainsKey(symbol))
+                {
+                    text.Add(MorseToText[symbol]);
+                }
+                else
+                { 
+                    throw new ArgumentException($"Морзе-код '{symbol}' не распознан.");
+                }
+            }
+            return new string(text.ToArray());
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Выберите режим:");
+                Console.WriteLine("1. Текст -> Морзе:");
+                Console.WriteLine("2. Морзе -> Текст:");
+                Console.WriteLine("Ваш выбор:");
+
+                string choice = Console.ReadLine();
+
+                try
+                {
+                    if (choice == "1")
+                    {
+                        Console.WriteLine("Введите текст: ");
+                        string input = Console.ReadLine();
+                        string morse = MorseTranslator.ToMorse(input);
+                        Console.WriteLine($"Азбука Морзе: {morse}");
+                    }
+                    else if (choice == "2")
+                    {
+                        Console.WriteLine("Введите Морзе используя пробел для разделения:");
+                        string input = Console.ReadLine();
+                        string text = MorseTranslator.FromMorse(input);
+                        Console.WriteLine($"Текст: {text}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("неверный выбор.");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static char player = 'X';
+        static bool isSinglePlayer = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в игру Крестики-Нолики!");
+            Console.WriteLine("Выберите режим:");
+            Console.WriteLine("1 - Играть с компьютером");
+            Console.WriteLine("2 - Играть с другим пользователем");
+
+            var choice = Console.ReadLine();
+            isSinglePlayer = choice == "1";
+
+            Random random = new Random();
+            if (random.Next(2) == 0)
+            {
+                Console.WriteLine("Игру начинает игрок X.");
+            }
+            else
+            {
+                player = 'O';
+                Console.WriteLine("Игру начинает игрок O.");
+            }
+
+            while (true)
+            {
+                Console.Clear();
+                DisplayBoard();
+                if (isSinglePlayer && player == 'O')
+                {
+                    ComputerMove();
+                }
+                else
+                {
+                    PlayerMove();
+                }
+
+                if (CheckWin())
+                {
+                    Console.Clear();
+                    DisplayBoard();
+                    Console.WriteLine($"Победил игрок {player}!");
+                    break;
+                }
+                if (CheckDraw())
+                {
+                    Console.Clear();
+                    DisplayBoard();
+                    Console.WriteLine("Ничья!");
+                    break;
+                }
+
+                player = player == 'X' ? 'O' : 'X';
+            }
+        }
+
+        static void DisplayBoard()
+        {
+            Console.WriteLine("-------------");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"| {board[i * 3]} | {board[i * 3 + 1]} | {board[i * 3 + 2]} |");
+                Console.WriteLine("-------------");
+            }
+        }
+
+        static void PlayerMove()
+        {
+            int move;
+            while (true)
+            {
+                Console.WriteLine($"Ход игрока {player}. Введите номер клетки:");
+                if (int.TryParse(Console.ReadLine(), out move) && move >= 1 && move <= 9 && board[move - 1] != 'X' && board[move - 1] != 'O')
+                {
+                    board[move - 1] = player;
+                    break;
+                }
+                Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+            }
+        }
+
+        static void ComputerMove()
+        {
+            Random random = new Random();
+            int move;
+            while (true)
+            {
+                move = random.Next(1, 10);
+                if (board[move - 1] != 'X' && board[move - 1] != 'O')
+                {
+                    board[move - 1] = player;
+                    Console.WriteLine($"Компьютер выбрал клетку {move}");
+                    break;
+                }
+            }
+        }
+
+        static bool CheckWin()
+        {
+            int[,] winConditions =
+            {
+                { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, // Горизонтальные
+                { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, // Вертикальные
+                { 0, 4, 8 }, { 2, 4, 6 } // Диагональные
+            };
+
+            for (int i = 0; i < winConditions.GetLength(0); i++)
+            {
+                if (board[winConditions[i, 0]] == player && board[winConditions[i, 1]] == player && board[winConditions[i, 2]] == player)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static bool CheckDraw()
+        {
+            foreach (var cell in board)
+            {
+                if (cell != 'X' && cell != 'O') return false;
+            }
+            return true;
+        }
+    }
+}
